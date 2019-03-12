c96aebd4c313cc0c7962c44b2f7f0275d96fb869
rodolfo <rodolfomayora96@gmail.com>
2019-03-12 14:33:42 -0400

    Doc: segunda alternativa con N imagenes

--- script.js
+++ script.js
@@ -2,35 +2,44 @@
  * @fileoverview gestion de slideshow de imagenes
  * @author rodolfo mayora
  */
+
 let // referencias DOM
-		img = document.querySelectorAll( '.img' ),
-		imgInterval = 4000;
+		images = document.querySelectorAll( '.img' ),
+		imgIndex = 0,
+		imgTotal = images.length,
+		imgInterval = 3000,
+		slideItems = [];
 
 /**
  * @function
- * @name setSlideImg
+ * @name changeImg
  * @description cambia las clases de la imagenes del carrusel
  */
-function setSlideImg() {
-	for ( let element of img ) {
-		let elementClass = element.classList;
-		if ( elementClass.contains( 'img--center' ) ) {
-			elementClass.toggle( 'img--center' );
-			elementClass.add( 'img--left' );
-			continue;
-		} 
+function changeImg( number ) {
+	images[ imgIndex ].classList.remove( 'img--center');
+	images[ imgIndex ].classList.add( 'img--left');
 
-		if ( elementClass.contains( 'img--left' ) ) {
-			elementClass.toggle( 'img--left' );
-			elementClass.add( 'img--right' );
-			continue;
-		} 
+	images[ number ].classList.remove( 'img--right');
+	images[ number ].classList.add( 'img--center');
 
-		if ( elementClass.contains( 'img--right' ) ) {
-			elementClass.toggle( 'img--right' );
-			elementClass.add( 'img--center' );
-			continue;
-		}
-	}
+	imgIndex = number;
 }
-window.setInterval( setSlideImg, imgInterval );
+
+function left() {
+	let left = document.querySelector( '.img--left' );
+	left.classList.remove( 'img--left' );
+	left.classList.add( 'img--right' );
+}
+
+function change() {
+	let a;
+	if ( imgIndex === imgTotal - 1 ) a = 0;
+	else a = imgIndex + 1;
+	left();
+	changeImg( a );
+}
+
+let carrusel = window.setInterval( change, imgInterval );
+
+// detener intervalo
+// clearInterval(carrusel)
