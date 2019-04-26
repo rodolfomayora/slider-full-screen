f0e14293831dc8c2275487527c60e85d7754f975
rodolfo <rodolfomayora96@gmail.com>
2019-04-26 10:17:57 -0400

    Refactor: optiizacon de js y css

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 /**
- * @fileoverview gestion de slideshow de imagenes
+ * @fileoverview gestion del SLIDER de imagenes
  * @author rodolfo mayora
  */
 
 let // referencias DOM
-		images = document.querySelectorAll( '.img' ),
-		//variables
-		imgTotal = images.length,
-		imgLast = imgTotal - 1,
-		imgIndexCenter = 0,
-		imgIndexRight = 1,
-		imgIndexLeft = imgLast,
-		imgInterval = 7000,
-		// una cosa experimental que no se para que lo puse alli
-		slideItems = [];
+	images = document.querySelectorAll( '.img' ),
+	//variables 
+	imgTotal = images.length,
+	imgLast = imgTotal - 1,
+	imgIndexCenter = 0,
+	imgIndexRight = 1,
+	imgIndexLeft = imgLast,
+	imgInterval = 7000,
+	isPause = false;
+
 
 /**
  * @function
  * @name changeImg
- * @description cambia las clases de la imagenes del carrusel
+ * @description cambia las clases de la imagenes del slider
  * @param {number} newLeft
  * @param {number} newCenter
+ * @param {number} newRight
  */
-function changeImg( newLeft, newCenter, newRight ) {
+function changeImg ( newLeft, newCenter, newRight ) {
 	images[ imgIndexLeft ].classList.remove( 'img--left' );
 
 	// lo puse en este orden por la cascada de CSS
@@ -41,17 +42,15 @@ function changeImg( newLeft, newCenter, newRight ) {
 } 
 
 
-
-let isPause = false;
-
 /**
  * @function
  * @name change
  * @description verifica el estado de los indices izquierdo y central
  */
-function change() {
+function change () {
 	
 	if ( !isPause ) {
+
 		let left = 0,
 				center = 0,
 				rignt = 0;
@@ -71,18 +70,25 @@ function change() {
 }
 
 
+/**
+ * @function
+ * @name setStatus
+ * @description cambia el estado si el usuario sale o vuelve a la ventana del site
+ */
 function setStatus () {
 	isPause = !isPause;
 }
 
-// detiene el carrusel cuando el usuario quita el foco de la pagina
-window.addEventListener( 'blur', setStatus );
 
-// reanuda el carrusel cuando el usuario vuelve a la pagina
-window.addEventListener( 'focus', setStatus );
-
-// inicia el carrusel solo cuando toda la pagina halla cargado
 window.addEventListener( 'load', function () {
+
+	// detiene el carrusel cuando el usuario quita el foco de la pagina
+	window.addEventListener( 'blur', setStatus );
+
+	// reanuda el carrusel cuando el usuario vuelve a la pagina
+	window.addEventListener( 'focus', setStatus );
+
+	// inicia el carrusel solo cuando toda la pagina halla cargado
 	let carrusel = window.setInterval( change, imgInterval );
 } )
 
