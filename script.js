f0239eb628dd634b0d72635bcd769bb5cd11eec9
rodolfo <rodolfomayora96@gmail.com>
2019-04-27 14:43:43 -0400

    Refactor: optimizacion del codigo de script.js

--- script.js
+++ script.js
@@ -3,16 +3,16 @@
  * @author rodolfo mayora
  */
 
-let // referencias DOM
+let  // referencias DOM
 	images = document.querySelectorAll( '.img' ),
 	//variables 
 	imgTotal = images.length,
 	imgLast = imgTotal - 1,
-	imgIndexCenter = 0,
 	imgIndexRight = 1,
+	imgIndexCenter = 0,
 	imgIndexLeft = imgLast,
-	imgInterval = 7000,
-	isPause = false;
+	imgInterval = 6000,
+	slider = null;
 
 
 /**
@@ -24,18 +24,21 @@ let // referencias DOM
  * @param {number} newRight
  */
 function changeImg ( newLeft, newCenter, newRight ) {
+	// la imagen que esta a la derecha sale del slider
 	images[ imgIndexLeft ].classList.remove( 'img--left' );
 
-	// lo puse en este orden por la cascada de CSS
+	// la imagen del centro se ubica a la derecha
 	images[ imgIndexCenter ].classList.add( 'img--left' );
 	images[ imgIndexCenter ].classList.remove( 'img--center' );
 
+	// la imange de de la derecha se ubica en el centro 
 	images[ imgIndexRight ].classList.add( 'img--center' );
 	images[ imgIndexRight ].classList.remove( 'img--right' );
 
+	// una imagen entra al carrusel
 	images[ newRight ].classList.add( 'img--right' );
 
-
+	// se actualizan los indices
 	imgIndexLeft = newLeft;
 	imgIndexCenter = newCenter;
 	imgIndexRight = newRight;
@@ -44,54 +47,44 @@ function changeImg ( newLeft, newCenter, newRight ) {
 
 /**
  * @function
- * @name change
+ * @name changePosition
  * @description verifica el estado de los indices izquierdo y central
  */
-function change () {
+function changePosition () {
 	
-	if ( !isPause ) {
+	let left = 0,
+			center = 0,
+			rignt = 0;
+			
+	if ( imgIndexLeft === imgTotal - 1 ) left = 0;
+	else left = imgIndexLeft + 1;
 
-		let left = 0,
-				center = 0,
-				rignt = 0;
-				
-		if ( imgIndexLeft === imgTotal - 1 ) left = 0;
-		else left = imgIndexLeft + 1;
+	if ( imgIndexCenter === imgTotal - 1 ) center = 0;
+	else center = imgIndexCenter + 1;
 
-		if ( imgIndexCenter === imgTotal - 1 ) center = 0;
-		else center = imgIndexCenter + 1;
+	if ( imgIndexRight === imgTotal - 1 ) right = 0;
+	else right = imgIndexRight + 1;
 
-		if ( imgIndexRight === imgTotal - 1 ) right = 0;
-		else right = imgIndexRight + 1;
-
-		changeImg( left, center, right );
-	}
+	changeImg( left, center, right );
 
 }
 
 
-/**
- * @function
- * @name setStatus
- * @description cambia el estado si el usuario sale o vuelve a la ventana del site
- */
-function setStatus () {
-	isPause = !isPause;
-}
-
-
+/* EVENTOS
+ ====================================================*/
 window.addEventListener( 'load', function () {
 
-	// detiene el carrusel cuando el usuario quita el foco de la pagina
-	window.addEventListener( 'blur', setStatus );
+	// detiene el slider cuando el usuario quita el foco de la pagina
+	window.addEventListener( 'blur', function () {
+		window.clearInterval( slider );
+	} );
 
-	// reanuda el carrusel cuando el usuario vuelve a la pagina
-	window.addEventListener( 'focus', setStatus );
+	// reanuda el slider cuando el usuario vuelve a la pagina
+	window.addEventListener( 'focus', function () {
+		slider = window.setInterval( changePosition, imgInterval );
+	} );
 
-	// inicia el carrusel solo cuando toda la pagina halla cargado
-	let carrusel = window.setInterval( change, imgInterval );
-} )
+	// inicia el slider solo cuando toda la pagina ha cargado
+	slider = window.setInterval( changePosition, imgInterval );
 
-
-// detener intervalo
-// clearInterval(carrusel)
+} );
